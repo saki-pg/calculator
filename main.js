df15b110c4b9ef8d6edac278bef5b81a5a9f00f4
saki-pg <115139489+saki-pg@users.noreply.github.com>
2022-11-01 00:46:19 +0900

    電卓課題修正（小数点）

--- main.js
+++ main.js
@@ -1,60 +1,137 @@
-// データ
-var result = "";
-// =で計算したかどうか
-var is_calc = false;
+'use strict'
+{
+  const num_bth = document.querySelectorAll('.num_bth');
+  let output_sub = document.getElementById('output_sub');
+  const output_total = document.getElementById('output_total');
+  let total = 0;
+  let state = 'start';
+  let mode = 'integer_mode';
+  //  変数modeに、整数入力中integer_mode、小数入力中decimal_modeを定義
 
-// 初期表示
-window.onload = function () {
-  result = document.getElementById('result');
-};
+  // 1-9の数字ボタンを押した時
+    const one_nine = document.querySelectorAll('.one_nine');
+    one_nine.forEach(index => {     
+      index.addEventListener('click', () => {
+        if(state === 'start') {
+          total = index.dataset.indexId;         
+        }else if(state === 'finish') {
+          reset();
+          total = index.dataset.indexId;  
+        }else if(state === 'calculation'||state === 'calBtn'){
+          total += index.dataset.indexId;
+        }     
+        output_sub.textContent = total;
+        state = 'calculation'
+        changeOutput()
+      }) 
+    })
 
-// Cキー押下
-function ac_click(){
-  result.value = "0";
-  is_calc = false;
-}
+  // 0の数字ボタンを押した時
+  const zero = document.getElementById('zero');
+  zero.addEventListener('click', () => {
+  if(state==='start'||state==='finish'||state==='calBtn'){
+      if(output_sub.textContent.slice(-1) === '0') {
+        console.log('前の文字はゼロ');
+        return;
+      }
+    }
 
-// 数字キー押下
-function num_click(val){
-  if(is_calc)  result.value = "0";
-  is_calc = false;  
+    if(state==='start') {
+      total = zero.dataset.indexId;  
+    }else{
+      total += zero.dataset.indexId;
+    }      
+    output_sub.textContent = total;
+    changeOutput()
+  }) 
 
-  if(result.value =="0" && val == "0"){
-    result.value = "0";
-  }else if(result.value == "0" && val == "."){
-    result.value = "0.";
-  }else if(result.value == "0"){
-    result.value = val;
-  }else{
-    result.value += val;
+  // 「.」小数点ボタンを押した時
+  const point = document.getElementById('point');
+  point.addEventListener('click', () => {
+    console.log(point.dataset.indexId)
+    if(mode === 'decimal_mode'){
+      return; //小数点入力モードではもう一度小数点を押せない
+       }
+       
+    if(state==='start'||state==='finish') {
+      total = 0;
+    }else if(state==='calBtn'){
+      if(output_sub.textContent.slice(-1)!=='0'){
+        total += 0;
+      }   
+    }
+    total += point.dataset.indexId;
+
+    output_sub.textContent = total;
+    state = 'calculation'//数字を入力している状態にする。
+    mode = 'decimal_mode'; //小数入力モードに変更
+    changeOutput()//計算結果・計算過程画面の入れ替える
+  }) 
+
+  //「＋　÷　－　×」ボタンを押した時
+  const cal = document.querySelectorAll('.cal');
+  cal.forEach(index => {     
+    index.addEventListener('click', () => {
+      if(state === 'start') {
+        return;//最初記号は押せない
+      }else if(state === 'calculation'){
+        total += index.dataset.indexId;//計算中はtotalに打った記号を追加し、totalに代入する。
+      }else if(state === 'finish'){
+        //計算後は前の計算結果をtotal に代入して計算しなおす。
+        total = output_total.textContent;
+        total += index.dataset.indexId;
+        output_total.textContent = 0
+      }else if(state ==='calBtn') {
+        total = total.slice(0, -1)
+        total += index.dataset.indexId;
+      }
+
+      output_sub.textContent = total;
+      state = 'calBtn'//演算記号を入力している状態する。
+      mode ='integer_mode'//整数モードに戻す
+      changeOutput()//計算結果・計算過程画面の入れ替える
+    })
+  })
+
+  //イコールを押した時
+  const equal_btn = document.getElementById('equal_btn');
+  equal_btn.addEventListener('click',() =>{
+    console.log(eval(total));
+    output_total.textContent = digitNum(eval(total));
+    state = 'finish'//計算が終わった状態にする。
+    mode ='integer_mode'//整数モードに戻す
+    changeOutput()//計算結果・計算過程画面の入れ替える
+  });
+
+  //ACボタン（リセットボタン）を押した時の処理
+  const clear = document.getElementById('clear')
+  clear.addEventListener('click', () => {
+    reset();
+  })
+
+ //リセットを行う関数
+  function reset() {
+    total = 0; 
+    output_sub.textContent = 0;
+    output_total.textContent = 0;
+    mode ='integer_mode'//整数モードに戻す
+    state ='start';
+    changeOutput()//計算結果・計算過程画面の入れ替える
   }
-}
 
-// 演算子キー押下
-function ope_click(val){
-  if(is_calc)  is_calc = false;
-  
-  if(is_ope_last()){
-    result.value = result.value.slice(0, -1) + val;
-  } else {
-    result.value += val;
+
+  function digitNum(num) {
+    return Math.round(num*100000000)/100000000;
   }
-}
 
-// =キークリック
-function equal_click(){
-  if(is_ope_last())  result.value = result.value.slice(0, -1);
-
-  var temp = new Function("return " + result.value.replaceAll("×", "*").replaceAll("÷", "/"))();
-  if(temp == Infinity || Number.isNaN(temp)){
-    result.value = "Error";
-  }else{
-    result.value = temp;
-    is_calc = true;
+  function changeOutput(){
+    if(state==='finish'){
+      output_total.classList.add('active');
+      output_sub.classList.remove('active');   
+    }else{
+      output_sub.classList.add('active');
+      output_total.classList.remove('active'); 
+    } 
   }
-}
 
-// 入力されている値が演算子かどうか
-function is_ope_last(){
-  return ["+","-","×","÷"].includes(result.value.toString().slice(-1));
 }
